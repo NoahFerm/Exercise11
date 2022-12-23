ea235705df2fd2c31c79f16b57fc255e27878460
Zxewl <noahferm@gmail.com>
2022-12-23 15:03:06 +0100

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿//using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public string Shelf { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Count { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
